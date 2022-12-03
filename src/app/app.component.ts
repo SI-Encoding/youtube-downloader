de1427a94a5375111d84f45d333707b2b77dfdcb
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-12-02 16:51:13 -0800

    Added error handling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,16 +27,27 @@ export class AppComponent {
 
   loading: boolean = false;
 
+  error: boolean = false;
+
+  errorMessage: string = "";
+
   constructor(private http: HttpClient) {}
     
     download(): void {
+        this.error = false;
+        this.errorMessage = "";
         this.loading = true;
         this.http.get<vidDetails>('http://localhost:5000/download', {params: this.input}).subscribe((data) => {
         this.video = true;
         this.url = data.url;
         this.info = data.info;
         this.loading = false;
-      });
+      }, (err) => {
+        this.loading = false;
+        this.error = true;
+        this.errorMessage = err.error.text;
+      }    
+    )
     }
     
     reset(): void {
